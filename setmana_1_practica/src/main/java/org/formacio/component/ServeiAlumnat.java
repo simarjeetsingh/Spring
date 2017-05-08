f55ffc158237f255179620e762afe88fbe116532
simar <simarjeetsingh2011@gmail.com>
2017-05-08 19:44:35 +0200

    Tests ok.

--- setmana_1_practica/src/main/java/org/formacio/component/ServeiAlumnat.java
+++ setmana_1_practica/src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -3,6 +3,8 @@ package org.formacio.component;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.test.annotation.Commit;
@@ -28,9 +30,38 @@ public class ServeiAlumnat {
 		}
 		
 	}
+	/*
+	@PostConstruct
 	public void insertarAlumnes() {
 		repositori.altaAlumne(1, "Antonia");
 		repositori.altaAlumne(2, "Joan");
 	}	
+	*/
+	
+	@PostConstruct
+	public void init(){
+		/* 
+		 * Open/Closed
+		 * i Single Responsability 
+		 * Principis SOLID 
+		 * */
+		this.inicialitzarRepositoriAlumnes();
+	}
+	
+	public void inicialitzarRepositoriAlumnes(){
+		
+		String[] alumnes = {"Antonia", "Joan"};
+		final byte POSICIO_INICIAL_REPOSITORI = 1;
+		
+		short posicio = POSICIO_INICIAL_REPOSITORI;
+		for(String alumne: alumnes){			
+			repositori.altaAlumne((int) posicio, alumne);
+			posicio += 1;
+		}
+	}
+	
+	public int nombreAlumnesRepositori(){
+		return repositori.llistaAlumnes().size();
+	} 
 	
 }
